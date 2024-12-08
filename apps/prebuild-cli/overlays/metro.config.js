1dcb85e0603711c72abe359455fb6192be396b31
Hassan Khan <h@twodoors.dev>
2024-12-08 21:58:11 +0000

    feat(apps/prebuild-cli): use Metro config from @rnx-kit

--- apps/prebuild-cli/overlays/metro.config.js
+++ apps/prebuild-cli/overlays/metro.config.js
@@ -1,7 +1,8 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
+const { makeMetroConfig } = require('@rnx-kit/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
-module.exports = config;
+module.exports = makeMetroConfig(config);
